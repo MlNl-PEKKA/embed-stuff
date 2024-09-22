2ebd70dcdb74e93737e84731c0d36e7304236715
Punith G <punithg@aglinthq.com>
2024-09-22 21:51:19 +0530

    set CORS headers in Middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,28 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { createClient } from "~/server/db";
 
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 // 1. Specify protected and public routes
 const publicRoutes = ["/", "/login"] as const;
 
 export async function middleware(req: NextRequest) {
+  // Check the origin from the request
+  const origin = req.headers.get("origin") ?? "";
+  const isAllowedOrigin = origin.startsWith("http://localhost");
+
+  const isPreflight = req.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
   // 2. Check if the current route is protected or public
   const path = req.nextUrl.pathname;
   const isPublicRoute = publicRoutes.includes(
@@ -16,11 +34,21 @@ export async function middleware(req: NextRequest) {
   if (isPublicRoute) {
     if (data.session && path === publicRoutes[1])
       return NextResponse.redirect(new URL("/home", req.nextUrl));
-    return NextResponse.next();
   } else {
-    if (data.session) return NextResponse.next();
-    return NextResponse.redirect(new URL("/login", req.nextUrl));
+    if (!data.session)
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
+  return response;
 }
 
 export const config = {
