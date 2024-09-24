983ead808778abded596cb939adb8f0dfcc130d4
Punith G <punithg@aglinthq.com>
2024-09-25 02:42:09 +0530

    null check db schema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type { DB } from "~/server/db/schema";
+
 export type CustomizableTypes<T extends "Array" | "Object"> = T extends "Array"
-  ? Record<string, any>[]
-  : Record<string, any>;
+  ? Record<string, unknown>[]
+  : Record<string, unknown>;
 
 export type Custom<
   T extends CustomizableTypes<"Array"> | CustomizableTypes<"Object">,
   U extends T extends CustomizableTypes<"Array">
-    ? Partial<{ [id in keyof T[number]]: any }>
-    : Partial<{ [id in keyof T]: any }>,
+    ? Partial<{ [id in keyof T[number]]: unknown }>
+    : Partial<{ [id in keyof T]: unknown }>,
 > =
   T extends CustomizableTypes<"Array">
     ? (Omit<T[number], keyof U> &
@@ -29,4 +31,60 @@ export type RequiredOnly<T extends CustomizableTypes<"Object">> = Pick<
 export type PartialOnly<T extends CustomizableTypes<"Object">> = Omit<
   T,
   keyof RequiredOnly<T>
+>;
+
+type ExcludeNullable<T> = T extends null ? never : T;
+
+type IgnoreNullable<T extends Record<string, unknown>> = {
+  [id in keyof T]: ExcludeNullable<T[id]>;
+};
+
+type NeverNullable<T extends Record<string, unknown>> = {
+  [id in keyof T]: null extends T[id] ? never : T[id];
+};
+
+type NeverNotNullable<T extends Record<string, unknown>> = {
+  [id in keyof T]: null extends T[id] ? T[id] : never;
+};
+
+type OmitNever<T extends Record<string, unknown>> = {
+  [id in keyof T as T[id] extends never ? never : id]: T[id];
+};
+
+type PickNullable<T extends Record<string, unknown>> = OmitNever<
+  NeverNotNullable<T>
+>;
+
+type OmitNullable<T extends Record<string, unknown>> = OmitNever<
+  NeverNullable<T>
+>;
+
+export type SelectiveNotNull<
+  T extends Record<string, unknown>,
+  U extends keyof PickNullable<T>,
+> = OmitNullable<T> &
+  Pick<IgnoreNullable<PickNullable<T>>, U> &
+  Omit<PickNullable<T>, U>;
+
+export type SelectiveNotNullTables<
+  T extends OmitNever<{
+    [id in keyof DB["public"]["Tables"]]: keyof PickNullable<
+      DB["public"]["Tables"][id]["Row"]
+    >;
+  }>,
+> = Custom<
+  DB["public"]["Tables"],
+  {
+    [id in keyof T]: Custom<
+      //@ts-expect-error
+      DB["public"]["Tables"][id],
+      {
+        Row: SelectiveNotNull<
+          //@ts-expect-error
+          DB["public"]["Tables"][id]["Row"],
+          T[id]
+        >;
+      }
+    >;
+  }
 >;
--- src/server/db/schema/index.ts
+++ src/server/db/schema/index.ts
@@ -1,10 +1,23 @@
 import type { Custom } from "~/lib/types";
 import type { Database } from "./db";
 import type { Tables } from "./tables";
+import type { NotNullableTables } from "./nullChecks";
 
 export type DB = Custom<
   Database,
   {
     public: Custom<Database["public"], { Tables: Tables }>;
+  }
+>;
+
+export type NullChecksDB = Custom<
+  DB,
+  {
+    public: Custom<
+      DB["public"],
+      {
+        Tables: NotNullableTables;
+      }
+    >;
   }
 >;
