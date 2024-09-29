b1e327269cf695c21a9d22f2b2657849b798a6f6
Punith G <punithg@aglinthq.com>
2024-09-29 23:05:48 +0530

    fix mode selection

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -23,11 +23,11 @@ export function ModeToggle() {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <TooltipTrigger>
-            <Button variant="ghost" size="icon">
+            <div className="inline-flex h-10 items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
               <Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
-            </Button>
+            </div>
           </TooltipTrigger>
         </DropdownMenuTrigger>
 
