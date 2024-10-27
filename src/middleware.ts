4984f031593be1576066d6de72b192fce48454fa
Punith G <punithg@aglinthq.com>
2024-10-27 12:31:19 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,65 +1,75 @@
 import { type NextRequest, NextResponse } from "next/server";
+
 import { createProtectedClient } from "~/server/db";
-
-const corsOptions = {
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
-// 1. Specify protected and public routes
-const publicRoutes = ["/", "/login", "/api/auth/callback"] as const;
-
-export async function middleware(req: NextRequest) {
-  // Check the origin from the request
-  const origin = req.headers.get("origin") ?? "";
-  const isAllowedOrigin = origin.startsWith("http://localhost");
-
-  const isPreflight = req.method === "OPTIONS";
-
-  if (isPreflight) {
-    const preflightHeaders = {
-      ...(isAllowedOrigin && { "Access-Control-Allow-Origin": origin }),
-      ...corsOptions,
-    };
-    return NextResponse.json({}, { headers: preflightHeaders });
-  }
-
-  // 2. Check if the current route is protected or public
-  const path = req.nextUrl.pathname;
-  const isPublicRoute = publicRoutes.includes(
-    path as (typeof publicRoutes)[number],
-  );
-  const db = createProtectedClient();
-  const { data, error } = await db.auth.getSession();
-  if (error) NextResponse.redirect(new URL("/error", req.nextUrl));
-  if (isPublicRoute) {
-    if (data.session && path === publicRoutes[1])
-      return NextResponse.redirect(new URL("/projects", req.nextUrl));
-  } else {
-    if (!data.session)
-      return NextResponse.redirect(new URL("/login", req.nextUrl));
-  }
-  const response = NextResponse.next();
-
-  if (isAllowedOrigin) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
-  }
-
-  Object.entries(corsOptions).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
-
-  return response;
-}
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*.svg$|sitemap.xml|robots.txt).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
+
+const PUBLIC_API = ["trpc", "auth/callback"].map(
+  (api) => `^/api/${api}(/.*)?$`,
+);
+
+const PUBLIC_ROUTES = new RegExp(["^/$", ...PUBLIC_API].join("|"));
+
+const PROTECTED_ROUTES = new RegExp(
+  ["^/emotes(/.*)?$", "^/projects(/.*)?$", "^/profile$"].join("|"),
+);
+
+const PROTECTED_DEFAULT = "/emotes";
+
+const SIGN_IN = "/login";
+
+export const middleware = async (request: NextRequest) => {
+  return (
+    (await preflightCheck(request)) ??
+    (await publicPageCheck(request)) ??
+    (await sessionCheck(request)) ??
+    NextResponse.redirect(new URL(SIGN_IN, request.nextUrl))
+  );
+};
+
+const preflightCheck: MiddlewareType = async (request) => {
+  const isPreflight = request.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders: HeadersInit = {
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    };
+
+    const origin = request.headers.get("origin") ?? "";
+
+    const isAllowedOrigin = origin.startsWith("http://localhost");
+
+    if (isAllowedOrigin)
+      preflightHeaders["Access-Control-Allow-Origin"] = origin;
+
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+};
+
+const publicPageCheck: MiddlewareType = async (request) => {
+  const { pathname } = request.nextUrl;
+
+  if (PUBLIC_ROUTES.test(pathname)) return NextResponse.next();
+};
+
+const sessionCheck: MiddlewareType = async (request) => {
+  const { pathname } = request.nextUrl;
+
+  const session = (await createProtectedClient().auth.getSession()).data
+    ?.session;
+
+  if (session) {
+    if (pathname === SIGN_IN || !PROTECTED_ROUTES.test(pathname))
+      return NextResponse.redirect(new URL(PROTECTED_DEFAULT, request.nextUrl));
+    return NextResponse.next();
+  } else if (pathname === SIGN_IN) return NextResponse.next();
+};
+
+type MiddlewareType<T extends NextRequest = NextRequest> = (
+  _req: T,
+) => Promise<NextResponse | undefined>;
