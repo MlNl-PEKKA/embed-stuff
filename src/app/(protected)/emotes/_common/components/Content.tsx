ac4218b1ea4d8941312209990ca1d85ab5887d1f
MlNl-PEKKA <punithg360@gmail.com>
2024-11-15 01:06:35 +0530

    content wrap

--- src/app/(protected)/emotes/_common/components/Content.tsx
+++ src/app/(protected)/emotes/_common/components/Content.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export const Content = (props: PropsWithChildren) => {
   return (
-    <div className="m-4 flex flex-grow flex-row items-start justify-start gap-4">
+    <div className="m-4 flex flex-grow flex-row flex-wrap items-start justify-start gap-4">
       {props.children}
     </div>
   );
