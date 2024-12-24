8d51bbcab298764168ee637e38368628ddd9aecf
MlNl-PEKKA <punithg360@gmail.com>
2024-12-24 20:43:39 +0530

    init + tooling setup

---
+++ toolings/tailwind/eslint.config.js
@@ -0,0 +1,6 @@
+// FIXME: This kinda stinks...
+/// <reference types="../../toolings/eslint/types.d.ts" />
+
+import baseConfig from "@embed-stuff/eslint-config/base";
+
+export default [...baseConfig];
