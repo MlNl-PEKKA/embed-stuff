33fc599c54af25b811e23cf36e67ef5da8240c57
MlNl-PEKKA <punithg360@gmail.com>
2024-12-27 03:37:07 +0530

    update callback

--- apps/web/src/app/api/auth/callback/route.ts
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -7,7 +7,7 @@ export const GET = async (request: Request) => {
   await connection();
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/projects";
+  const next = searchParams.get("next") ?? "/feedbacks";
 
   const url = await api.auth.callback({ code, next, origin });
 
